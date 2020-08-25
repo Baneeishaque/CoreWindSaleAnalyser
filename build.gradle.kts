5d4d5b0b1cc89d3dfe340cd75babf7ebd845b441
Banee Ishaque K <baneeishaque@gmail.com>
2020-08-25 06:15:09 +0530

    In corewindA

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,17 @@
-plugins {
-    java
-}
-
-group = "snake"
-version = "1.0-SNAPSHOT"
-
-repositories {
-    mavenCentral()
-}
-
-dependencies {
-    testImplementation("junit", "junit", "4.12")
-}
+//plugins {
+//    java
+//}
+//
+//group = "snake"
+//version = "1.0-SNAPSHOT"
+//
+//repositories {
+//    mavenCentral()
+//}
+//
+//dependencies {
+//    testImplementation("junit", "junit", "4.12")
+//}
+//TODO : Customize distTar task to customize distribution file name
+//TODO : Create windows, mac, linux, etc. installation files
+//TODO : Include jre in distribution & executable files
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,2 @@
 rootProject.name = "CoreWindSaleAnalyser"
-
+include("corewindA")
